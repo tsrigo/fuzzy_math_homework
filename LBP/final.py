c9f5f3814962d5266ab285ad569303a995448b18
tsrigo <717660037@qq.com>
2022-07-26 12:26:10 +0800

    实现，获取对于不同 lambda 所对应的截断矩阵

--- LBP/final.py
+++ LBP/final.py
@@ -7,8 +7,8 @@ def get_img(dirPath):
 
 def get_closure(r):
     '''
-    返回由模糊相似矩阵 r(n * n) 得到的模糊等价矩阵(传递闭包) R
-    c_ij = ∨{(r_ik ∧ r_kj) | 1 ≤ k ≤ n}.
+    返回由模糊相似矩阵得到的模糊等价矩阵(传递闭包) R
+    其中R_ij = ∨{(r_ik ∧ r_kj) | 1 ≤ k ≤ n}.
     '''
     n = len(r)
     R = np.zeros((n, n))
@@ -20,14 +20,31 @@ def get_closure(r):
     return R
 
 def get_range(R):
+    '''根据模糊等价矩阵中的隶属度，确定一系列用以分类的lamda。'''
     res = []
     getRound = lambda x: round(x, 2)
     for i in R:
         res.extend(getRound(x) for x in i)
-    return set(res)
+    return sorted(list(set(res)))
 
+def get_truncation_matrix(R, lam):
+    '''
+    根据给定的 lambda，返回 R 的λ截矩阵
+    '''
+    res = []
+    def cmp(x):
+        if x >= lam: 
+            return 1
+        else: 
+            return 0
+    for row in R:
+        res.append([cmp(x) for x in row])
+    return res
 
-
+def putMat(R):
+    for i in R:
+        print(i)
+    print()
 
 dirPath = "/data/1/"
 
@@ -39,10 +56,10 @@ for root, dirs, files in os.walk(file_dir, topdown=False):
         loc = root + '\\' + f
         feature.append(ULBP.get_LBPH(loc))
             
-n = len(feature)    # height of feature
-m = len(feature[0]) # width of feature
-r = np.zeros((n, n))     # similarity matrix
-M = 85
+n = len(feature)    # 高，Height of feature
+m = len(feature[0]) # 宽，Width of feature
+r = np.zeros((n, n))# 相似矩阵，Similarity matrix
+M = 85              # 绝对值倒数法的系数，Coefficients of the Reciprocal Absolute Value Method
 
 for i in range(n):     
     for j in range(n): 
@@ -54,6 +71,13 @@ for i in range(n):
 print("\n所有 r_ij 是否均位于 [0, 1] 区间内：" + str(all([x <= 1 and x >= 0 for i in r for x in i])) + '\n')
 
 R = get_closure(r)
-# print(R)
+# print("模糊等价矩阵为：")
+# putMat(R)
 ran = get_range(R)
-print(ran)
+print("lambda 的取值有：" + str(ran))
+
+for lam in ran:
+    tMat= get_truncation_matrix(R, lam)
+    print(str(lam) + " 对应的截断矩阵为：")
+    putMat(tMat)
+
